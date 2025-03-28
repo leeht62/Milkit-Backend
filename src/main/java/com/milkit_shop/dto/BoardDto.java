59dc9125ec8b6e9a19171ea493d5a9ff84e4295b
leeht62 <zdfxd3611@naver.com>
2025-03-29 00:20:53 +0900

    milkit

--- src/main/java/com/milkit_shop/dto/BoardDto.java
+++ src/main/java/com/milkit_shop/dto/BoardDto.java
@@ -5,17 +5,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class BoardDto {
   private Long id;
+  private LocalDateTime date;
   private String title;
   private String content;
+  private String userEmail;
 
   public BoardDto(Board board) {
     this.id = board.getId();
     this.title = board.getTitle();
     this.content = board.getContent();
+    this.date=LocalDateTime.now();
+    this.userEmail = board.getMember().getEmail();
   }
 }

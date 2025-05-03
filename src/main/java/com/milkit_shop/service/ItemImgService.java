da0f1c4b2a2ef9150ceff7df59902cc32bf6a990
leeht62 <zdfxd3611@naver.com>
2025-05-03 22:19:32 +0900

    milkit

--- src/main/java/com/milkit_shop/service/ItemImgService.java
+++ src/main/java/com/milkit_shop/service/ItemImgService.java
@@ -1,8 +1,11 @@
 package com.milkit_shop.service;
 
+import com.milkit_shop.entity.Item;
 import com.milkit_shop.entity.ItemImg;
 import com.milkit_shop.repository.ItemImgRepository;
+import com.milkit_shop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +20,7 @@ public class ItemImgService {
   private String itemImgLocation;
   private final ItemImgRepository itemImgRepository;
   private final FileService fileService;
+  private final ItemRepository itemRepository;
 
   public void saveItemImg(ItemImg itemImg, MultipartFile itemImgFile)throws Exception{
     String oriImgName=itemImgFile.getOriginalFilename();
@@ -35,6 +39,8 @@ public class ItemImgService {
     if(!itemImgFile.isEmpty()){
       ItemImg savedItemImg=itemImgRepository.findById(itemImgId)
           .orElseThrow(EntityNotFoundException::new);
+      Item item=itemRepository.findById(savedItemImg.getItem().getId())
+          .orElseThrow(EntityNotFoundException::new);
       if(savedItemImg.getImgName() != null && !savedItemImg.getImgName().isBlank()){
         fileService.deleteFile(itemImgLocation+"/"+savedItemImg.getImgName());
       }
@@ -42,6 +48,7 @@ public class ItemImgService {
       String imgName= fileService.uploadFile(itemImgLocation,oriImgName,itemImgFile.getBytes());
       String imgUrl="/images/item/"+imgName;
       savedItemImg.updateItemImg(oriImgName,imgName,imgUrl);
+      item.setImage(savedItemImg.getImgUrl());
     }
 
 

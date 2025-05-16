47a2b407aedbd556b7ceb9473cc89b7cc95ccd93
leeht62 <zdfxd3611@naver.com>
2025-05-17 06:32:55 +0900

    milkit

--- src/main/java/com/milkit_shop/config/S3Config.java
+++ src/main/java/com/milkit_shop/config/S3Config.java
@@ -1,18 +1,24 @@
 package com.milkit_shop.config;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
 
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
     @Bean
     public AmazonS3 amazonS3() {
         return AmazonS3ClientBuilder
             .standard()
-            .withRegion("ap-northeast-2") // 서울 리전
+            .withRegion(region)
+            .withCredentials(DefaultAWSCredentialsProviderChain.getInstance()) // 이 부분 중요!
             .build();
     }
 }

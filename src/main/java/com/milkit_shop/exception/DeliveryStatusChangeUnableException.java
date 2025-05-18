30bc0a23f82b1454ff23382541e8975272927814
Kyuseok Park <dosim0106@gmail.com>
2025-05-18 19:32:22 +0900

    add exception

---
+++ src/main/java/com/milkit_shop/exception/DeliveryStatusChangeUnableException.java
@@ -0,0 +1,7 @@
+package com.milkit_shop.exception;
+
+public class DeliveryStatusChangeUnableException extends RuntimeException {
+    public DeliveryStatusChangeUnableException(String message) {
+        super(message);
+    }
+}

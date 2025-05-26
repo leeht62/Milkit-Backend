a35869595920febda2f8eaf0f05802f0d5eb3e73
Kyuseok Park <dosim0106@gmail.com>
2025-05-26 18:06:33 +0900

    out_of_stock exception handling

--- src/main/java/com/milkit_shop/exception/DeliveryStatusChangeUnableException.java
+++ src/main/java/com/milkit_shop/exception/DeliveryStatusChangeUnableException.java
@@ -1,6 +1,14 @@
 package com.milkit_shop.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class DeliveryStatusChangeUnableException extends RuntimeException {
+
+    private final HttpStatus status = HttpStatus.BAD_REQUEST;
+    private final String code = "DELIVERY_STATUS_CHANGE_UNABLE";
+
     public DeliveryStatusChangeUnableException(String message) {
         super(message);
     }

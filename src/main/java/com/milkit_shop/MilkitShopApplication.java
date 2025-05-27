51752c3032b6bc79e8fe35a7bd0e48b1ed8edc45
Kyuseok Park <dosim0106@gmail.com>
2025-05-27 19:00:58 +0900

    localtiem setting

--- src/main/java/com/milkit_shop/MilkitShopApplication.java
+++ src/main/java/com/milkit_shop/MilkitShopApplication.java
@@ -3,11 +3,12 @@ package com.milkit_shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MilkitShopApplication {
-
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(MilkitShopApplication.class, args);
 	}
-
 }
